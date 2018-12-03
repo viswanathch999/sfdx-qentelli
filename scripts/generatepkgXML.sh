0ecc933e773015369ede2db5c7de9aed0dd73f52
viswanathch999 <viswanathch456@gmail.com>
2018-12-03 16:03:40 +0000

    changed

--- scripts/generatepkgXML.sh
+++ scripts/generatepkgXML.sh
@@ -2,20 +2,19 @@
 
 if [ $# -lt 1 ]
 then
-    echo Usage: generatepkgXML.sh orgalias packageName
+    echo Usage: generatepkgXML.sh orgalias pack
     exit
 fi
 
 ## Retrieve the PackageXML from Unmanaged Container
 
+rm -rf ./mdapipkg
+
+mkdir ./mdapipkg/
+
 sfdx force:mdapi:retrieve -s -r ./mdapipkg -u $1 -p "$2" # Retrieve Metadata API Source from Package Name
 
 unzip -o -qq ./mdapipkg/unpackaged.zip -d ./mdapipkg # Unzip the file
 
-rm -rf ./manifest/ # If manifest directory exists delete it
-
-mkdir ./manifest/ # Create a New Manifest Directory
-
-cp -a ./mdapipkg/package.xml ./manifest/ # Copy package.XML to manifest directory
 
 
