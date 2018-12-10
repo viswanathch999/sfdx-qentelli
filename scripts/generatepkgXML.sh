5fc066434e75bf4293b60af288a713328214e910
viswanathch999 <viswanathch456@gmail.com>
2018-12-10 06:12:54 +0000

    Added changeset

--- scripts/generatepkgXML.sh
+++ scripts/generatepkgXML.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ]
 then
-    echo Usage: generatepkgXML.sh orgalias pack
+    echo Usage: generatepkgXML.sh orgalias package
     exit
 fi
 
@@ -15,5 +15,9 @@ mkdir ./mdapipkg/
 sfdx force:mdapi:retrieve -s -r ./mdapipkg -u $1 -p "$2" # Retrieve Metadata API Source from Package Name
 
 unzip -o -qq ./mdapipkg/unpackaged.zip -d ./mdapipkg # Unzip the file
+
+
+
+
 
 
